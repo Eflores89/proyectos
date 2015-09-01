d479fbba7a71625de23804a05aec01204db343a8
Eduardo Flores <eduardo@enelmargen.org>
2015-09-01 16:12:59 -0500

    ecopred final

--- scripts/ecopred.R
+++ scripts/ecopred.R
@@ -263,12 +263,13 @@ df_mod <- inner_join(P_medidas,
                      P_mudarte, by = "KEY_U") %>%
   inner_join(., P_sucede, by = "KEY_U") %>% 
   select(c(2:17,27,67:91)) %>%
-  select(-KEY)
+  select(-c(KEY)
 
 # hacer factores a character para manipular.
 i <- sapply(df_mod, is.factor)
 df_mod[i] <- lapply(df_mod[i], as.character)
 
+# eliminar los que están vacios.
 df_mod[,c(1:16)][is.na(df_mod[,c(1:16)])]<-0
 
 # Información generada de si o no...
@@ -277,15 +278,28 @@ df_mod$P5_3 <- vapply(df_mod$P5_3,
                       function(x) {if(x=="1" | x=="2" | x=="3" | x=="4")
                         {1} else {0}}, 
                       FUN.VALUE=double(1)) 
-# mientras
-names(df_mod)<-c(names(df_mod)[1:6],"VENTA_DROGA", names(df_mod)[8:12],"ROBANDO_CASA","ASALTO_PERSONA",names(df_mod)[15:41])
 
+# cambiar nombres 
+names(df_mod)<-c(names(df_mod)[1:6],
+                 "VENTA_DROGA", 
+                 names(df_mod)[8:12],
+                 "ROBANDO_CASA","ASALTO_PERSONA",
+                 names(df_mod)[15:41])
+
+# volver a factorizar para modelo...
 i <- sapply(df_mod, is.character)
 df_mod[i] <- lapply(df_mod[i], as.factor)
 
+# primero un arbolito, 
+un_arbol <- rpart(formula = as.factor(P5_3) ~ .,
+                  data = df_mod)
+
+# un bosque...
 modelo <- randomForest(as.factor(P5_3) ~ .,
                        data = df_mod,
                        importance = TRUE, 
                        ntree = 2000)
+# graficar las importancias...
+randomForest::varImpPlot(modelo, n.var = 20)
 
-importancia <- modelo$importance
+
